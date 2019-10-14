cf1e75955149248e3cf377c7255790712c560b8c
Francesco Cosentino <fc@hyperd.sh>
2019-10-14 23:14:19 +0300

    builds update

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ build () {
 
 push_to_scm() {
   git status
-  git add . ; git commit -m "builds update" ; git push || :
+  git add -A . ; git commit -m "builds update" || : ; git push || :
 }
 
 update_docker_images () {
