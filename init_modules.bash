7b0f42f53dedf26def47ebdda8d2e0198bffe2fb
Francesco Cosentino <fc@hyperd.sh>
2019-10-15 01:30:01 +0300

    modules update

--- init_modules.bash
+++ init_modules.bash
@@ -42,7 +42,7 @@ ini_modules () {
     git add -A . ; git commit -m "modules update" || : ; git push || :
 
     for i in "${modules[@]}"; do
-        cd $i ; rm -rf go.* ; cd - #; go mod init ; go get ; go mod tidy ; go build ; cd -
+        cd $i ; rm -rf go.* ; go mod init ; go get ; go mod tidy ; go build ; cd -
     done
 
     report
