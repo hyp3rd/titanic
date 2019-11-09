7966782f9e64972dda494aeb7eb803a00f5a7b1f
Francesco Cosentino <fc@hyperd.sh>
2019-11-09 10:53:34 +0200

    modules update

--- people.go
+++ people.go
@@ -2,22 +2,19 @@ package titanic
 
 import (
 	"context"
-	"errors"
-	"regexp"
 
 	"github.com/google/uuid"
-	"github.com/jinzhu/gorm"
 )
 
 // People represents a single passenger (People).
 // UUID should be globally unique.
 type People struct {
 	// gorm.Model
-	ID                    uuid.UUID `json:"uuid,omitempty" gorm:"primary_key"`
-	Survived              bool      `json:"survived,omitempty" valid:"bool"`
+	ID                    uuid.UUID `json:"uuid,omitempty" gorm:"primary_key" valid:"uuid"`
+	Survived              bool      `json:"survived,omitempty"`
 	Pclass                int       `json:"pclass,omitempty" valid:"numeric"`
-	Name                  string    `json:"name,omitempty" valid:"length(2|48)"`
-	Sex                   string    `json:"sex,omitempty"`
+	Name                  string    `json:"name,omitempty" valid:"alphanum,stringlength(2|48)"`
+	Sex                   string    `json:"sex,omitempty" valid:"in(male|female|not declared)"`
 	Age                   int       `json:"age,omitempty" valid:"numeric"`
 	SiblingsSpousesAbroad int       `json:"siblings_spouses_abroad,omitempty" valid:"numeric"`
 	ParentsChildrenAboard int       `json:"parents_children_aboard,omitempty" valid:"numeric"`
@@ -25,24 +22,24 @@ type People struct {
 }
 
 // Validate People struct. All the error can be catched with `db.GetErrors()`
-func (people People) Validate(db *gorm.DB) {
-	nameIsCorrect, _ := regexp.MatchString(`^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$`, people.Name)
-	if !nameIsCorrect {
-		db.AddError(errors.New("Name: invalid format"))
-	}
-	if !isValidSex(people.Sex) {
-		db.AddError(errors.New("Sex: invalid value"))
-	}
-	if people.Age < 0 || people.Age > 116 {
-		db.AddError(errors.New("Age: value in invalid range"))
-	}
-	if people.SiblingsSpousesAbroad < 0 || people.SiblingsSpousesAbroad > 20 {
-		db.AddError(errors.New("SiblingsSpousesAbroad: value in invalid range"))
-	}
-	if people.ParentsChildrenAboard < 0 || people.ParentsChildrenAboard > 20 {
-		db.AddError(errors.New("ParentsChildrenAboard: value in invalid range"))
-	}
-}
+// func (people People) Validate(db *gorm.DB) {
+// 	nameIsCorrect, _ := regexp.MatchString(`^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$`, people.Name)
+// 	if !nameIsCorrect {
+// 		db.AddError(errors.New("Name: invalid format"))
+// 	}
+// 	if !isValidSex(people.Sex) {
+// 		db.AddError(errors.New("Sex: invalid value"))
+// 	}
+// 	if people.Age < 0 || people.Age > 116 {
+// 		db.AddError(errors.New("Age: value in invalid range"))
+// 	}
+// 	if people.SiblingsSpousesAbroad < 0 || people.SiblingsSpousesAbroad > 20 {
+// 		db.AddError(errors.New("SiblingsSpousesAbroad: value in invalid range"))
+// 	}
+// 	if people.ParentsChildrenAboard < 0 || people.ParentsChildrenAboard > 20 {
+// 		db.AddError(errors.New("ParentsChildrenAboard: value in invalid range"))
+// 	}
+// }
 
 // Repository describes the persistence on people model
 type Repository interface {
@@ -55,13 +52,13 @@ type Repository interface {
 }
 
 // Validation helpers
-func isValidSex(a string) bool {
-	list := [...]string{"male", "female", "not declared", ""}
+// func isValidSex(a string) bool {
+// 	list := [...]string{"male", "female", "not declared", ""}
 
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
+// 	for _, b := range list {
+// 		if b == a {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
